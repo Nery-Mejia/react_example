f37c98d8dca6a4e0b2cc68234cf7f11b05386f59
Nery Mejia <MD22007@ues.edu.sv>
2023-10-08 17:47:06 -0600

    Definiendo enum con rutas de la aplicacion

---
+++ app/utils/constants.ts
@@ -0,0 +1,8 @@
+export enum urlDeAplicacion
+{
+    urlnicio = '/',
+    urlExamples = '/examples',
+    urlExample1 = '/examples/1',
+    urlExample2 = '/examples/2',
+    urlExample3 = '/examples/3',
+}
