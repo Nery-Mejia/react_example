c78b6c47f52e1f1bc09432e8059c48966b4f7402
Nery Mejia <MD22007@ues.edu.sv>
2023-10-08 17:38:01 -0600

    Implementando componente UseCard

--- app/examples/1/page.tsx
+++ app/examples/1/page.tsx
@@ -1,12 +1,19 @@
-import Image from 'next/image';
+import UserCard from '@/components/UserCard/index';
 import React, { HTMLAttributes } from 'react'
+import { IUserCardPropiedades } from '@/components/UserCard';
 
-interface IUsuario {
+
+export interface IUsuario {
   id: number;
   firstName: string;
   lastName: string;
   picture: string;
   role: 'Admin' | 'Client';
+}
+
+import { Metadata } from 'next'
+export const metadata: Metadata = {
+    title: 'Ejemplo 1: Renderizar lista de objetos',
 }
 
 const users:IUsuario[] = [
@@ -52,25 +59,11 @@ const Example1 = () => {
     <div className='flex flex-col gap-4 items-center'>
       <h1 className='text-2xl font-bold'>Renderizar una lista de objetos</h1>
       {
-        users.map(
-          (user, index) => {
-            return (
-              <div key = {user.id} className='flex flex-row gap-4 border border-gray-300 rounded-lg overflow-hidden w-96'>
-                <div className=' aspect-square w-32'>
-                  <Image width={128} height={128} className=' object-cover w-full h-full' src={user.picture} alt={`User photo of ${user.firstName} ${user.lastName}`} />
-                </div>
-                <div className='flex flex-col justify-center'>
-                  <span><span className='font-bold'>Nombre:</span> {user.firstName}</span>
-                  <span><span className='font-bold'>Apellido:</span> {user.lastName}</span>
-                  <span><span className='font-bold'>Rol:</span> {user.role}</span>
-                </div>
-              </div>
-            )
-          }
-        )
+        users.map((user, index) => (
+          <UserCard key={user.id} user={user} />
+        ))
       }
     </div>
-  )
-}
-
+  );
+};
 export default Example1
